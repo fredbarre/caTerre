aac1123017c13cdc834941b12d157a68d501be18
fred <fredchoix@protonmail.com>
2023-04-19 16:36:31 +0200

    exo12

---
+++ terre11.js
@@ -0,0 +1,22 @@
+let args = process.argv;
+let textDate = args[2];
+let splitDate = textDate.split(":");
+let hours = parseInt(splitDate[0]);
+let mins = parseInt(splitDate[1]);
+
+let ushours;
+
+let AMPM;
+if (hours <= 11) {
+  AMPM = "AM";
+} else {
+  AMPM = "PM";
+}
+
+if (hours <= 12 && hours >= 1) ushours = hours;
+
+if (hours >= 13 && hours <= 23) ushours = hours - 12;
+
+if (hours == 0) ushours = 12;
+
+console.log(ushours + ":" + mins + AMPM);
